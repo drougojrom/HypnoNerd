bdb540ce1d369a0ed948d8f058ab11dc0db8207b
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-23 23:34:49 +0300

    added random labels and hideKeyboard

--- HypnosisViewController.m
+++ HypnosisViewController.m
@@ -9,7 +9,42 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController () <UITextFieldDelegate>
+@end
+
 @implementation HypnosisViewController
+
+-(void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0;i<20;i++){
+        UILabel *messageLabel = [[UILabel alloc] init];
+        
+        // configure message color and text
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        
+        [messageLabel sizeToFit];
+        
+        // get a random x value of the width
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+        
+        // get random y value of the height
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+        
+        // update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        // add the label to hierarchy
+        [self.view addSubview:messageLabel];
+        
+        
+    }
+}
 
 -(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -52,10 +87,21 @@
     textField.placeholder = @"Hypnotize me!";
     textField.returnKeyType = UIReturnKeyDone;
     
+    textField.delegate = self;
+    
     [backgroundView addSubview:textField];
     
     // set it as "the" view of the view controller
     self.view = backgroundView;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
